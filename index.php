b507187cdf099455090a0b153d2f7e81d33a7ecb
wiraphong-boat <wiraphong.ch@rmuti.ac.th>
2018-08-31 18:03:40 +0700

    index

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+<body class="w3-light-grey">
+<div class="w3-light-grey" style="max-width:1400px">
 <style>
- h1{
-     color: red;
- }
+ h1 { color: #202020 ;
+      font-family: "Raleway", sans-serif;}
 </style>
-<head>
-  <h1>Hello World</h1>
-<body>
-    
+ <div>
+  <h1 style = "text-align: center;" ><b>Computer science</b></h1>
+  </div>
+  
+  <div class="w3-bar w3-black w3-card">
+  <div class="w3-dropdown-hover w3-hide-small">
+  <button class="w3-padding-large w3-button" title="More">News</button>
+  <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="https://www.facebook.com/paboatindy" class="w3-bar-item w3-button">facebook</a>
+        <a href="https://medium.com/@paboatindy55" class="w3-bar-item w3-button">Medium</a>  
+  </div>
+  </div>
+  </div>
+
+   
+</div>
 </body>
 </html>
